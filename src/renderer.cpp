b2393b14f508f94f60dd78c3e0e9076f012617d4
Nora Beda <theo.github@bedafamily.com>
2021-11-18 16:47:19 -0800

    add creation of debug messenger in the most elegant way possible

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -21,7 +21,51 @@ namespace vkrollercoaster {
     static struct {
         std::vector<const char*> instance_extensions, layer_names;
         VkInstance instance = nullptr;
+        VkDebugUtilsMessengerEXT debug_messenger = nullptr;
     } renderer_data;
+    static VKAPI_ATTR VkBool32 VKAPI_CALL validation_layer_callback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+        VkDebugUtilsMessageTypeFlagsEXT message_type,
+        const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+        void* user_data) {
+        std::string message = "validation layer: " + std::string(callback_data->pMessage);
+        switch (message_severity) {
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            spdlog::warn(message);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            spdlog::error(message);
+            throw std::runtime_error("vulkan error encountered");
+        default:
+            // not important enough to show
+            break;
+        };
+        return VK_FALSE;
+    }
+#ifdef NDEBUG
+    constexpr bool enable_validation_layers = false;
+#else
+    constexpr bool enable_validation_layers = true;
+#endif
+    static bool check_layer_availability(const std::vector<const char*>& required_layers) {
+        uint32_t layer_count;
+        vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+        std::vector<VkLayerProperties> available_layers(layer_count);
+        vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+        for (const char* layer_name : required_layers) {
+            bool layer_found = false;
+            for (const auto& layer : available_layers) {
+                if (strcmp(layer.layerName, layer_name) == 0) {
+                    layer_found = true;
+                    break;
+                }
+            }
+            if (!layer_found) {
+                return false;
+            }
+        }
+        return true;
+    }
     static void choose_extensions() {
         uint32_t glfw_extension_count = 0;
         const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
@@ -29,6 +73,16 @@ namespace vkrollercoaster {
             for (uint32_t i = 0; i < glfw_extension_count; i++) {
                 renderer_data.instance_extensions.push_back(glfw_extensions[i]);
             }
+        }
+        if (enable_validation_layers) {
+            const std::vector<const char*> validation_layers = {
+                "VK_LAYER_KHRONOS_validation"
+            };
+            if (!check_layer_availability(validation_layers)) {
+                throw std::runtime_error("validation layers requested but not supported!");
+            }
+            renderer_data.instance_extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+            renderer_data.layer_names.insert(renderer_data.layer_names.begin(), validation_layers.begin(), validation_layers.end());
         }
     }
     static void create_instance() {
@@ -51,15 +105,43 @@ namespace vkrollercoaster {
             create_info.ppEnabledLayerNames = renderer_data.layer_names.data();
         }
         if (vkCreateInstance(&create_info, nullptr, &renderer_data.instance) != VK_SUCCESS) {
-            throw std::runtime_error("could not create vulkan instance!");
+            throw std::runtime_error("could not create a vulkan instance!");
         }
-        spdlog::info("created vulkan instance");
+    }
+    static void create_debug_messenger() {
+        if (!enable_validation_layers) {
+            // in release mode, a debug messenger would only slow down the application
+            return;
+        }
+        VkDebugUtilsMessengerCreateInfoEXT create_info;
+        util::zero(create_info);
+        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        create_info.pfnUserCallback = validation_layer_callback;
+        auto fpCreateDebugUtilsMessengerEXT = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(renderer_data.instance, "vkCreateDebugUtilsMessengerEXT");
+        if (fpCreateDebugUtilsMessengerEXT != nullptr) {
+            if (fpCreateDebugUtilsMessengerEXT(renderer_data.instance, &create_info, nullptr, &renderer_data.debug_messenger) != VK_SUCCESS) {
+                throw std::runtime_error("could not create debug messenger");
+            }
+        } else {
+            spdlog::warn("could not get vkCreateDebugUtilsMessengerEXT function address");
+        }
     }
     void renderer::init() {
         choose_extensions();
         create_instance();
+        create_debug_messenger();
     }
     void renderer::shutdown() {
+        if (renderer_data.debug_messenger != nullptr) {
+            auto fpDestroyDebugUtilsMessengerEXT = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(renderer_data.instance, "vkDestroyDebugUtilsMessengerEXT");
+            if (fpDestroyDebugUtilsMessengerEXT != nullptr) {
+                fpDestroyDebugUtilsMessengerEXT(renderer_data.instance, renderer_data.debug_messenger, nullptr);
+            } else {
+                spdlog::warn("created debug messenger but could not destroy it - will leak memory");
+            }
+        }
         vkDestroyInstance(renderer_data.instance, nullptr);
     }
 };
