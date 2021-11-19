167eed8f707fecca8c04c5eb0c6d5afcda01a5f5
Nora Beda <theo.github@bedafamily.com>
2021-11-18 17:43:48 -0800

    i give up

--- scripts/install_vulkan_sdk.py
+++ scripts/install_vulkan_sdk.py
@@ -1,6 +1,5 @@
 from os import unlink
 from requests import get
-from tempfile import TemporaryDirectory
 from sys import platform, argv
 from subprocess import call
 import os.path as path
@@ -12,15 +11,14 @@ def main():
     vulkan_sdk_version = response.content.decode("utf-8")
     installer_url = f"https://sdk.lunarg.com/sdk/download/{vulkan_sdk_version}/windows/VulkanSDK-{vulkan_sdk_version}-Installer.exe"
     response = get(installer_url, allow_redirects=True)
-    tempdir = TemporaryDirectory()
-    installer_path = path.join(tempdir.name, "vulkan-installer.exe")
+    installer_path = path.join(path.curdir, "vulkan-installer.exe")
     with open(installer_path, "wb") as stream:
         stream.write(response.content)
         stream.close()
-    if call([ installer_path, "/S" ], shell=True) != 0:
-        return 1
-    unlink(installer_url)
-    tempdir.cleanup()
+    return_code = call([ installer_path, "/S" ], shell=True)
+    unlink(installer_path)
+    if return_code != 0:
+        return 2
     if len(argv) > 1:
         if argv[1] == "--gh-actions":
             print(f"::set-env name=VULKAN_SDK::C:\\VulkanSDK\\{vulkan_sdk_version}")
