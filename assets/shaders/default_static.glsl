3337b44e76f9d5afad6cbaafe6cf067a846a98e0
Nora Beda <theo.github@bedafamily.com>
2021-11-28 19:36:22 -0800

    add shader structures

--- assets/shaders/default_static.glsl
+++ assets/shaders/default_static.glsl
@@ -26,6 +26,49 @@ layout(set = 1, binding = 0) uniform material_data {
     vec3 albedo_color;
 } material;
 layout(set = 1, binding = 1) uniform sampler2D albedo_texture;
+struct directional_light {
+    vec3 direction, color;
+};
+struct point_light {
+    vec3 position, color;
+    float _constant, _linear, _quadratic;
+};
+struct spotlight {
+    vec3 position, direction, color;
+    float cutoff, outer_cutoff;
+};
+layout(set = 1, binding = 2) uniform light_data {
+    int spotlight_count, point_light_count, directional_light_count;
+    directional_light directional_lights[30];
+    point_light point_lights[30];
+    spotlight spotlights[30];
+} lights;
+vec3 calculate_spotlight(int index, vec3 texture_color) {
+    spotlight light = lights.spotlights[index];
+    // todo: calculate spotlight
+    return vec3(0.0);
+}
+vec3 calculate_point_light(int index, vec3 texture_color) {
+    point_light light = lights.point_lights[index];
+    // todo: calculate point light
+    return vec3(0.0);
+}
+vec3 calculate_directional_light(int index, vec3 texture_color) {
+    directional_light light = lights.directional_lights[index];
+    // todo: calculate directional light
+    return vec3(0.0);
+}
 void main() {
-    color = texture(albedo_texture, uv) * vec4(material.albedo_color, 1.0);
+    vec4 texture_color = texture(albedo_texture, uv);
+    vec3 fragment_color = vec3(0.0);
+    for (int i = 0; i < lights.spotlight_count; i++) {
+        fragment_color += calculate_spotlight(i, texture_color.rgb);
+    }
+    for (int i = 0; i < lights.point_light_count; i++) {
+        fragment_color += calculate_point_light(i, texture_color.rgb);
+    }
+    for (int i = 0; i < lights.directional_light_count; i++) {
+        fragment_color += calculate_directional_light(i, texture_color.rgb);
+    }
+    color = vec4(fragment_color, texture_color.a);
 }
